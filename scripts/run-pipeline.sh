52cdf2d26e6577a093012e1ec3d3e4e52cc1619e
YotamEN <50518691+YotamEN@users.noreply.github.com>
2020-05-29 22:46:44 +0300

    reset

--- scripts/run-pipeline.sh
+++ scripts/run-pipeline.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-chmod +x scripts/run_dots.sh scripts/install.sh scripts/run_docker.sh
+chmod +x scripts/run_dots.sh scripts/install.sh scripts/run_docker.sh scripts/reset.sh
 scripts/run_docker.sh
 scripts/install.sh
