6969cc6e63e6cefe8781f68f2afe638b24e33970
YotamEN <yotam.evennir@gmail.com>
2020-05-29 21:45:16 +0300

    missing files found

---
+++ abra/errors.py
@@ -0,0 +1,19 @@
+
+class UnsupportedMessageQueueError(Exception):
+    pass
+
+
+class UnsupportedDataBaseError(Exception):
+    pass
+
+
+class UnsupportedTopic(Exception):
+    pass
+
+
+class UnknownParserError(Exception):
+    pass
+
+
+class UnknownPathError(Exception):
+    pass
