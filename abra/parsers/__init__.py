d029c6269f89c5155d62f64ab82658efc4f75b07
YotamEN <50518691+YotamEN@users.noreply.github.com>
2020-05-30 17:26:46 +0300

    small bug

--- abra/parsers/__init__.py
+++ abra/parsers/__init__.py
@@ -12,15 +12,16 @@ def parse(parser, data_path):
         raise UnknownParserError(f"Unknown parser! please choose one of the following:\n{PARSER_NAMES!r}")
 
     path = Path(data_path)
-    with open(path, "rb") as f:
-        raw_data = json.load(f)
+#     with open(path, "rb") as f:
+#         raw_data = json.load(f)
 
-    data = json.loads(raw_data)
+#     data = json.loads(raw_data)
     parser_h = get_parser(parser)
 
-    user_id, snapshot_id, path = data[USER_ID_KEY], data[SNAPSHOT_ID_KEY], data[USER_SNAPSHOTS_URL_KEY]
+#     user_id, snapshot_id, path = data[USER_ID_KEY], data[SNAPSHOT_ID_KEY], data[USER_SNAPSHOTS_URL_KEY]
     snapshot = load_snapshot(path)
     parsed_data = parser_h.parse(msg=snapshot)
+    user_id, snapshot_id = snapshot[USER_ID_KEY], snapshot[SNAPSHOT_ID_KEY]
     wrapped_parsed_data = wrap_parsed_data(parsed_data, parser, user_id, snapshot_id)
     parsed_data_json = json.dumps(wrapped_parsed_data)
     return parsed_data_json
